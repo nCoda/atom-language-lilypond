f0caf1ff47083a6f8ce56a28bcc8313959a2f256
Christopher Antila <christopher@antila.ca>
2018-07-11 21:56:47 -0400

    Initial commit

---
+++ spec/language-lilypond-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageLilypondView from '../lib/language-lilypond-view';
+
+describe('LanguageLilypondView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
