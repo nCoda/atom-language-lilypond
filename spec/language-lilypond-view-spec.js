29ce135a9896f94d51f508ca295ae36cd16dad52
Christopher Antila <christopher@antila.ca>
2018-07-15 21:19:25 -0400

    Remove unnecessary files

--- spec/language-lilypond-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import LanguageLilypondView from '../lib/language-lilypond-view';
-
-describe('LanguageLilypondView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
